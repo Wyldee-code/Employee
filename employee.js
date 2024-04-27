df0bc64f39e9d7d817757efd28a96506dea866b2
farvastra <farvastra@gmail.com>
2024-04-27 16:51:15 -0400

    manager2

--- employee.js
+++ employee.js
@@ -4,8 +4,18 @@ class Employees {
     this.salary = salary;
     this.title = title; 
     this.manager = manager;
+   
+    if (manager){
+      manager.addEmployee(this);
     }
-}
+  }
+    calculateBonus (multiplier){
+       return (this.salary) * multiplier;
+      }
+  }
+
+
+
 
 try {
     module.exports = Employees;
--- manager.js
+++ manager.js
@@ -2,10 +2,28 @@ const Employees = require('./employee')
 class Manager extends Employees {
     constructor (name, salary, title, manager = null){
         super (name, salary, title, manager)
-        this.employee = [];
+        this.employees = [];
     }
-addEmployees (employee){
-    this.employee.push(employee)
+    addEmployee (employee){
+        this.employees.push(employee)
+    }
+    calculateBonus (multiplier){
+        return (this.salary + this._totalSubSalary()) * multiplier;
+     }
+    
+    _totalSubSalary(){
+        let sum = 0;
+        for(const employee of this.employees){
+            if (employee instanceof Manager){
+            sum += employee.salary + employee._totalSubSalary()
+        } else{
+            sum += employee.salary
+        }  
+        
+        return sum;
+    }
+    
+        
 }
 }
 try {
